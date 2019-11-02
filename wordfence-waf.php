de5169777d193d87e216d1026e98e18f430e7beb
hyeakki <hye.akshay@gmail.com>
2019-11-02 20:16:04 +0000

    new

--- wordfence-waf.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
-
-if (file_exists('/usr/app/wp-content/plugins/wordfence/waf/bootstrap.php')) {
-	define("WFWAF_LOG_PATH", '/usr/app/wp-content/wflogs/');
-	include_once '/usr/app/wp-content/plugins/wordfence/waf/bootstrap.php';
-}
-?>
